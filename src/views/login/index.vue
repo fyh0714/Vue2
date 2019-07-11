fbe46e2b2f5a5f56902e5974a7a8ebd3e9af02bb
fyh <52880201@qq.com>
2019-07-12 00:25:38 +0800

    侧边栏完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,13 +2,77 @@
     <div class="login-container">
         <el-card class="login-box">
             <img src="../../assets/images/logo_index.png" alt="">
+            <!-- 登录表单 -->
+            <el-form ref="loginForm" :model="loginForm" :rules="loginRules">
+                <el-form-item prop="mobile">
+                    <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+                </el-form-item>
+                <el-form-item prop="code">
+                    <el-input v-model="loginForm.code" placeholder="请输入验证码" style="width:240px"></el-input>
+                    <el-button style="float:right">发送验证码</el-button>
+                </el-form-item>
+                <el-form-item>
+                  <el-checkbox v-model="checked"><span>我已阅读并同意</span>用户协议<span>和</span>隐私条款</el-checkbox>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" style="width:100%" @click="login()">登 录</el-button>
+                </el-form-item>
+            </el-form>
         </el-card>
     </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    const checkMobile = (rule, value, callback) => {
+      // 校验逻辑   把value拿出来进行手机号的格式校验
+      if (/^1[3-9]\d{9}/.test(value)) {
+        callback()
+      } else {
+        callback(new Error('手机号格式错误'))
+      }
+    }
+    return {
+      checked: true,
+      // 表单数据对象
+      loginForm: {
+        mobile: '',
+        code: ''
+      },
+      // 表单验证规则对象
+      loginRules: {
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          // validator:自定义校验
+          { validator: checkMobile, trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { len: 6, message: '请输入6位数字', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    login () {
+      this.$refs.loginForm.validate((valid) => {
+        if (valid) {
+          // 提交登录请求，axios是基于promise封装的 post() 返回值一个promise对象
+          this.$http.post('http://ttapi.research.itcast.cn/mp/v1_0/authorizations', this.loginForm)
+            .then((res) => {
+              // res是响应对象 包含后台返回的数据 res.data
+              // TODO 保存用户的信息 用来判断登录状态
+              // 登录成功跳转到首页
+              this.$router.push('/')
+            }).catch(() => {
+              // 提示
+              this.$message.error('手机号或验证码错误')
+            })
+        }
+      })
+    }
+  }
 }
 </script>
 
@@ -24,7 +88,7 @@ export default {
     background:url(../../assets/images/login_bg.jpg) no-repeat center / cover;
     .login-box {
     width: 400px;
-    height: 300px;
+    height: 340px;
     position: absolute;
     left: 50%;
     top: 50%;
@@ -33,6 +97,9 @@ export default {
         display:block;
         width: 200px;
         margin: 10px auto;
+     }
+     span {
+         color:rgb(121, 120, 120);
      }
     }
   }
