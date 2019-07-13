14b3cbe494cb8486bcef2757fa7436f02a50ddaf
fyh <52880201@qq.com>
2019-07-13 14:25:57 +0800

    配置基准路径&token认证

--- src/main.js
+++ src/main.js
@@ -11,11 +11,19 @@ import 'element-ui/lib/theme-chalk/index.css'
 import router from '@/router'
 
 import axios from 'axios'
+// 基准路径
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0/'
+// token认证
+axios.defaults.headers = {
+  Authorization: 'Bearer ' + JSON.parse(window.sessionStorage.getItem('hm74-toutiao')).token
+}
 Vue.prototype.$http = axios
+
 Vue.use(ELEMENT)
 
 Vue.config.productionTip = false
-
+// 职责1.导入项目需要依赖的资源（包、css ...）
+// 职责2.  初始化根实例 new Vue()选项render作用是把某一个App组件渲染在你指定的容器(.$mount('#app'))内
 new Vue({
   router,
   render: h => h(App)
